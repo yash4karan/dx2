d27240a922e1fa6e05a8a150998cc65a54e57421
Yash Karan <yash.karan@diamond.ac.uk>
2025-03-13 11:12:54 +0000

    Add a bunch of stuff when porting from dials

--- dx2/predict.cxx
+++ dx2/predict.cxx
@@ -1,4 +1,6 @@
 #include <dx2/experiment.h>
+//#include <dx2/reflection.h>
+
 // #include <fmt/color.h>
 // #include <fmt/core.h>
 // #include <fmt/os.h>
@@ -6,16 +8,31 @@
 
 #include <argparse/argparse.hpp>
 #include <chrono>
-// #include <dx2/h5/h5read_processed.hpp>
 // essential; common.hpp is in ffs => predict.cxx cannot be build on dx2 alone
 #include <common.hpp>
 #include <cstdlib>
-#include <dx2/h5/h5write.hpp>
+// #include <dx2/h5/h5read_processed.hpp>
+// #include <dx2/h5/h5write.hpp>
 #include <exception>
+#include <filesystem>
 #include <fstream>
 #include <iostream>  // Debugging
 #include <thread>
 #include <vector>  // Unused so far...
+
+// FIXME This is just a placeholder class to give an intution on how reflection tables work.
+// It is desigened only to support the API used by dx2.predict
+class ReflectionTable {
+  public:
+    ReflectionTable() = default;
+    ReflectionTable(std::string& h5_filepath) {}
+    void add_table(ReflectionTable refl);
+};
+
+template <typename T>
+ReflectionTable predict_from_expt(Experiment<T> expt, bool static_predict, float dmin) {
+    return ReflectionTable();
+}
 
 /**
  * @brief Takes a default-initialized ArgumentParser object and configures it 
@@ -25,7 +42,13 @@
  * @param parser The ArgumentParser object (pre-input) to be configured.
  */
 void configure_parser(argparse::ArgumentParser& parser) {
-    parser.add_argument("-e", "--expt").help("Path to DIALS expt file").required();
+    parser.add_argument("-e", "--expt")
+      .help("Path to input experiment file")
+      .required();
+    parser.add_argument("-o", "--output")
+      .help("Path to output reflection file")
+      .default_value("predicted.refl")
+      .implicit_value("predicted.refl");
     parser.add_argument("--dmin")
       .help("Minimum d-spacing of predicted reflections")
       .scan<'f', float>()
@@ -62,7 +85,7 @@ void configure_parser(argparse::ArgumentParser& parser) {
  * 
  * @param parser The ArgumentParser object (post-input) to be verified.
  */
-void verify_arguments(const argparse::ArgumentParser& parser) {
+void verify_parser_arguments(const argparse::ArgumentParser& parser) {
     if (!parser.is_used("expt")) {
         logger->error("Must specify experiment list file with --expt\n");
         std::exit(1);
@@ -72,7 +95,11 @@ void verify_arguments(const argparse::ArgumentParser& parser) {
         logger->error("Must specify --dmin\n");
         std::exit(1);
     }
-    if (parser.is_used("nthreads") && parser.get<size_t>("nthreads") < 1) {
+    if (parser.get<int>("buffer_size") < 0) {
+        logger->error("--buffer_size cannot be less than 0\n");
+        std::exit(1);
+    }
+    if (parser.get<size_t>("nthreads") < 1) {
         logger->error("--nthreads cannot be less than 1\n");
         std::exit(1);
     }
@@ -80,82 +107,81 @@ void verify_arguments(const argparse::ArgumentParser& parser) {
 
 int main(int argc, char** argv) {
     auto t1 = std::chrono::system_clock::now();
+    // Declare and configure the parser, then parse the arguments and verify them for consistency.
     auto parser = argparse::ArgumentParser();
     configure_parser(parser);
-
     try {
         parser.parse_args(argc, argv);
     } catch (const std::exception& err) {
         logger->error(err.what());
         std::exit(1);
     }
-
-    verify_arguments(parser);
+    verify_parser_arguments(parser);
 
     // Obtain argument values from the command line
     auto expt_path = parser.get<std::string>("expt");
+    auto output_path = parser.get<std::string>("output");
     auto dmin = parser.get<float>("dmin");
     auto static_predict = parser.get<bool>("static_predict");
     auto dynamic_shadows = parser.get<bool>("dynamic_shadows");
-    auto buffer_size = parser.get<size_t>("buffer_size");
+    auto buffer_size = parser.get<int>("buffer_size");
     auto nthreads = parser.get<size_t>("nthreads");
 
     // FIXME: What do the two macros below mean?
-    // hid_t file = H5Fopen(expt_path.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
+    // h5read_handle file = H5Fopen(expt_path.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT);
     // std::cout << file << '\n';
 
-    // Flatten experiments into a 1D array
+    // FIXME: Flatten experiments into a 1D array
     /*
     experiments = flatten_experiments(params.input.experiments)
     */
+    // FIXME: At this stage, assume that `experiments` has type std::vector<Experiment>
+    auto experiments = std::vector<Experiment<float>>(100);
 
     // If length of experiments in 0, print a help message and exit
-    /*
-    if len(experiments) == 0:
-        parser.print_help()
-        return
-    */
+    if (!experiments.size()) {
+        logger->error("Experiment list is empty.\n");
+        std::exit(1);
+    }
 
     // Create an empty reflection table to store predictions in
     /*
     predicted_all = reflection_table()
     */
+    // FIXME: This is a placeholder for the actual reflection table.
+    ReflectionTable predicted_all;
 
     // Populate `predicated_all` with predictions
-    /*
-    for i_expt, expt in enumerate(experiments):
-    */
+    for (int i = 0; i < experiments.size(); ++i) {
+        Experiment expt{experiments[i]};
+        // Predict reflections outside the range of the scan if buffer_size > 0
+        if (buffer_size > 0) {
+            // FIXME: This code assumes that `scan()` returns a reference to the _scan object in expt; this is NOT the case at the time of writing.
+            // FIXME: This code assumes that there exists a `set_image_range()` method in Scan; this is NOT the case at the time of writing.
+            std::array<int, 2> image_range{expt.scan().get_image_range()};
+            std::array<double, 2> oscillation{expt.scan().get_oscillation()};
+            expt.scan().set_image_range(
+              {image_range[0] - buffer_size, image_range[1] + buffer_size});
+            expt.scan().set_oscillation(
+              {oscillation[0] - buffer_size * oscillation[1], oscillation[1]});
+        }
 
-    // Predict reflections outside the range of the scan if buffer_size > 0
-    /*
-        if params.buffer_size > 0:
-            expt.scan.set_image_range(
-                (
-                    expt.scan.get_image_range()[0] - params.buffer_size,
-                    expt.scan.get_image_range()[1] + params.buffer_size,
-                )
-            )
-            expt.scan.set_oscillation(
-                (
-                    expt.scan.get_oscillation()[0] - params.buffer_size * oscillation[1],
-                    expt.scan.get_oscillation()[1],
-                )
-            )
-        */
-
-    // Create reflection table using `expt`, `params.force_static`, `params.d_min`
-    /*
+        // Write prediction algorithm here...
+        // Create reflection table using `expt`, `params.force_static`, `params.d_min`
+        /*
         predicted = flex.reflection_table.from_predictions(
             expt, force_static=params.force_static, dmin=params.d_min
         )
         */
+        ReflectionTable predicted = predict_from_expt(expt, static_predict, dmin);
 
-    // Copy experiment identifiers verbatim
-    /*
-        predicted.experiment_identifiers()[i_expt] = experiments[i_expt].identifier
-        predicted["id"] = flex.int(len(predicted), i_expt)
-        predicted_all.extend(predicted)
-        */
+        // FIXME: This code assumes resonable API for `predicted` and `predicted_all`. These classes do not exist yet.
+        // FIXME: This code assumes that the Experiment class contains a method `identifier()`. These classes do not exist yet.
+        predicted.experiment_identifiers()[i] = experiments[i].identifier();
+        // FIXME: The "id" might have different API; the data type of the "id" might be different.
+        predicted["id"] = std::array<int, 2>{len(predicted), i};
+        predicted_all.add_table(predicted);
+    }
 
     // If not ignoring shadows, look for reflections in the masked region
     /*
@@ -173,21 +199,24 @@ int main(int argc, char** argv) {
         )
         predicted_all = predicted_all.select(~shadowed)
     */
+    if (dynamic_shadows) {
+        // FIXME: Assuming that `experiments` is a Experiment-dictionary that is already conformant with JSON
+    }
 
     // Try to find bounding boxed for each experiment
-    /*
-    try:
-        predicted_all.compute_bbox(experiments)
-    except Exception:
-        pass
-    */
+    // FIXME: `ReflectionTable` does not have a member function `compute_bbox()` as of now.
+    try {
+        predicted_all.compute_bbox(experiments);
+    } catch (std::exception& err) {
+        logger->info(err.what());
+        continue;
+    }
 
     // Save reflections to file
-    /*
-    Command.start(f"Saving {len(predicted_all)} reflections to {params.output}")
-    predicted_all.as_file(params.output)
-    Command.end(f"Saved {len(predicted_all)} reflections to {params.output}")
-    */
+    logger->info("Saving {} reflections to {}", predicted_all.size(), output_path);
+    // FIXME: The following call needs to be modified as the dataset path is included within the Reflection Table. An overload will do.
+    write_data_to_h5_file(output_path, dataset_path, predicted_all);
+    logger->info("Saved {} reflections to {}", predicted_all.size(), output_path);
 
     auto t2 = std::chrono::system_clock::now();
     std::chrono::duration<double> elapsed_time = t2 - t1;
